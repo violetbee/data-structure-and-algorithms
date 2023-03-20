013f30675458e4e40f49a20526368f07351e1e7c
Violetbee <caglarkarahuseyin28@gmail.com>
2023-03-20 13:57:38 +0300

    dist updates, foobar test

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,25 @@
-console.log('HEY');
+import type { Foo } from './types/foo';
+
+const foo: Foo = {
+  bar: 'baz',
+};
+
+// Gelen array'deki elemanları küçükten büyüğe sırala
+let x = 1;
+console.log('Outside: ' + x);
+function myFunction() {
+  // console.log("Inside before re-declaring: " + x)
+  let x = 2;
+  console.log('Inside after re-declaring: ' + x);
+  if (x == 2) {
+    let x = 3;
+    console.log('Inside IF : ' + x);
+    function mySecondFunction() {
+      // console.log("Inside-Inside before: " + x)
+      let x = 4;
+      console.log('Inside-Inside after: ' + x);
+    }
+    mySecondFunction();
+  }
+}
+myFunction();
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
 "use strict";
-console.log('deneme');
+console.log('HEY');
